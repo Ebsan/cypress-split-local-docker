cc61d3cc8186a77dfd98760210875dee4aae7fde
Ebsan <6202912+Ebsan@users.noreply.github.com>
2024-10-29 17:21:09 +0000

    Add windows specific socketPath

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const axios = require('axios');
 const cypressSplit = require('cypress-split');
 const { determineSplit, determineSplitIndex } = require('./services/split');
+const os = require('os');
 
 const cypressSplitLocalDocker = async (on, config) => {
   if (process.env.LOCAL === 'true') {
-    const containers = (await axios.get(
-      'http://localhost/v1.41/containers/json',
-      { socketPath: '/var/run/docker.sock' },
-    )).data;
+    let socketPath = '/var/run/docker.sock';
+    const platform = os.platform();
+    if (platform.startsWith('win')) {
+      socketPath = '//./pipe/docker_engine';
+    }
+
+    const containers = (
+      await axios.get('http://localhost/v1.41/containers/json', {
+        socketPath: socketPath,
+      })
+    ).data;
     const containerId = process.env.HOSTNAME;
     // eslint-disable-next-line no-param-reassign
     config.env.split = determineSplit(containers, containerId);
